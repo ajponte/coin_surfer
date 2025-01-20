e86f54e69d29016737031024eaf679416e67390f
Alan Ponte <alanjponte@gmail.com>
2025-01-20 03:47:14 -0800

    add portfolio response to swagger (#4)

--- api/server/controllers/portfolio.py
+++ api/server/controllers/portfolio.py
@@ -1,14 +1,14 @@
-from typing import Dict
+import json
+from typing import Dict, Tuple
 
 from clients.coinbase import configure_coinbase
 
 
 DEFAULT_API_KEY_FILE = "etc/coinbase/api_key/cdp_api_key-ro.json"
 
-def get_portfolio(wallet_type: str, portfolio_id: str) -> Dict:
+def get_portfolio(wallet_type: str, portfolio_id: str) -> Tuple[Dict, int]:
     if wallet_type not in ['coinbase']:
-        return {'message': f'{wallet_type} not currently supported'}
+        return {'message': f'{wallet_type} not currently supported'}, 400
     coinbase = configure_coinbase(config={'COINBASE_KEY_FILE': DEFAULT_API_KEY_FILE})
     portfolios = coinbase.get_portfolio_breakdown(portfolio_id=portfolio_id)
-    import pdb; pdb.set_trace()
-    return {}
+    return {'data': json.dumps(portfolios.to_dict())}, 200
